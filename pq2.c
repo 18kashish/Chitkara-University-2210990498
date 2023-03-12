968bde2980e71d91158cb123cba31cf0407668d4
18kashish <124666183+18kashish@users.noreply.github.com>
2023-03-12 16:52:33 +0530

    Add files via upload

---
+++ pq2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int schl;
+	printf("enter the schl");
+	scanf("%d",&scope
+	+);
+	if(scope==8)
+	{
+		printf("use snipper gun");
+	}
+	else if(scope==6)
+	{
+		printf("use AUG A3,GROZA,QBZ,M16A4,M4");
+	}
+	else if(scope==4)
+	{
+		printf("use UMP9,AKM,SAR-L,Cross Bow");
+	}
+	else if(scope==2)
+	{
+		printf("almost all guns");
+	}
+	else
+	{
+		printf("find one");
+	}
+}
