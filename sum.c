ec93f1f64a4c25a9463f616ce5b3c8974ea78c75
18kashish <124666183+18kashish@users.noreply.github.com>
2023-03-12 17:09:25 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a1[100],a2[100],b,n,i,s=0,c;
+	printf("\nenter the number");
+	scanf("%d",&n);
+	printf("\narray 1");
+	for(i=0;i<n;i++)
+	{
+		printf("\nelements-%
+    
+}
